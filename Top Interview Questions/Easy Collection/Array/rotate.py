247ef758065cca68f860b5875332090207610c30
Adrian Molofsky <molofsky@stanford.edu>
2023-07-27 12:43:45 -0700

    Create rotate.py

---
+++ Top Interview Questions/Easy Collection/Array/rotate.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+"""
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """ 
+        if (k > len(nums)): k = k % len(nums)
+        nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k] 
+            
