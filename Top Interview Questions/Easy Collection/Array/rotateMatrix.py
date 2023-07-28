5a580374eb3105193ff09f1bfcf32c3199c4f6d7
Adrian Molofsky <molofsky@stanford.edu>
2023-07-28 15:20:39 -0700

    Create rotateMatrix.py

---
+++ Top Interview Questions/Easy Collection/Array/rotateMatrix.py
@@ -0,0 +1,19 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix 
+and do the rotation.
+"""
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        
+        for i in range(len(matrix)):
+            matrix[i] = matrix[i][::-1]
