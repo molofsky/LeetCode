634d2999a84b52e2a2a7b448d94d659c87a5d89f
Adrian Molofsky <molofsky@stanford.edu>
2023-08-01 14:05:52 -0700

    Create firstUniqChar.py

---
+++ Top Interview Questions/Easy Collection/Strings/firstUniqChar.py
@@ -0,0 +1,18 @@
+"""
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+"""
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = {}
+        
+        for char in s:
+            if char not in count:
+                count[char] = 1
+            else:
+                count[char] += 1
+                
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
+        return -1
