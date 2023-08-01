390cfa6ec4aa9accc1618ad037d80806a807450c
Adrian Molofsky <molofsky@stanford.edu>
2023-08-01 14:37:32 -0700

    Create isPalindrome.py

---
+++ Top Interview Questions/Easy Collection/Strings/isPalindrome.py
@@ -0,0 +1,11 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c for c in s.lower() if c.isalnum())
+        return s == s[::-1]
