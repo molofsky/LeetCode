8ae538c254c3d9bd28416340cbf5788077380888
Adrian Molofsky <molofsky@stanford.edu>
2024-06-18 23:32:19 -0700

    Update rotateArray.py

--- Top Interview Questions/Easy Collection/Array/rotateArray.py
+++ Top Interview Questions/Easy Collection/Array/rotateArray.py
@@ -10,3 +10,24 @@ class Solution:
         if (k > len(nums)): k = k % len(nums)
         nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k] 
             
+        """
+        Solution 2
+
+        k = k % len(num)
+        nums[:] = nums[-k:] + nums[:-k]
+
+        Solution 3
+        
+        n = len(nums)
+        k = k % n
+        
+        def reverse(arr, start, end):
+            while start < end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+                
+        nums.reverse()
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, n - 1)
+        """
