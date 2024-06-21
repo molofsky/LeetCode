962b0c092fadb802aba3a0581c400d44c617886b
Adrian Molofsky <molofsky@stanford.edu>
2024-06-20 20:09:47 -0700

    Update moveZeros.py

--- Top Interview Questions/Easy Collection/Array/moveZeros.py
+++ Top Interview Questions/Easy Collection/Array/moveZeros.py
@@ -14,4 +14,21 @@ class Solution:
             if (nums[i] != 0):
                 nums[j], nums[i] = nums[i], nums[j]
                 j+=1
+        """
+        Solution #2
         
+        for i in range(len(nums)):
+            if (nums[i] == 0):
+                nums.remove(nums[i])
+                nums += [0]
+        Solution #3
+
+        index_nonzero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index_nonzero] = nums[i]
+                index_nonzero+=1
+                
+        for j in range(index_nonzero, len(nums)):
+            nums[j] = 0
+        """
