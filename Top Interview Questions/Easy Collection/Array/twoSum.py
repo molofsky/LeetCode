fa28a05d8af95943698c5d6a45528163dbd1f99a
Adrian Molofsky <molofsky@stanford.edu>
2024-06-20 21:04:08 -0700

    Update twoSum.py

--- Top Interview Questions/Easy Collection/Array/twoSum.py
+++ Top Interview Questions/Easy Collection/Array/twoSum.py
@@ -16,4 +16,21 @@ class Solution:
                     res = [i, j] if i < j else [j, i]
                     break
         return res
+
+        """
+        Solution #2
+
+        nums2 = [target - num for num in nums]
+        j = 0
         
+        for i in range(len(nums)):
+                if nums[i] in nums2[:i] + nums2[i+1:]:
+                    j = (nums2[:i] + nums2[i+1:]).index(nums[i])
+                    if (j >= i): j = j + 1
+                    if i > j:
+                        return [j, i]
+                    else:
+                        return [i, j]
+                
+        return []
+        """
