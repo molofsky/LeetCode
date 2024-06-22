eec7f51d3e3594b3d49ba0b41cede85396c82ce8
Adrian Molofsky <molofsky@stanford.edu>
2024-06-21 21:41:02 -0700

    Update rotateMatrix.py

--- Top Interview Questions/Easy Collection/Array/rotateMatrix.py
+++ Top Interview Questions/Easy Collection/Array/rotateMatrix.py
@@ -10,10 +10,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+
+        # Transpose
         for i in range(len(matrix)):
             for j in range(i, len(matrix)):
                 matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-        
+
+        # Reversing Rows
         for i in range(len(matrix)):
             matrix[i] = matrix[i][::-1]
