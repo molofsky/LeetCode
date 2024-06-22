65a59eefcf16c8ffe938a7b9f4a188971554fa34
Adrian Molofsky <molofsky@stanford.edu>
2024-06-21 22:01:05 -0700

    Update rotateMatrix.py

--- Top Interview Questions/Easy Collection/Array/rotateMatrix.py
+++ Top Interview Questions/Easy Collection/Array/rotateMatrix.py
@@ -19,3 +19,19 @@ class Solution:
         # Reversing Rows
         for i in range(len(matrix)):
             matrix[i] = matrix[i][::-1]
+
+
+"""
+Side Problem: rotate the matrix counter-clockwise 
+"""
+class Solution:
+    def rotate_counter(self, matrix: List[List[int]]) -> None:
+        # Transpose
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+        # Reverse Columns
+        for i in range(len(matrix));
+            for j in range(len(matrix) // 2):
+                matrix[j][i], matrix[len(matrix) - j - 1][i] = matrix[len(matrix) - j - 1][i], matrix[j][i]
