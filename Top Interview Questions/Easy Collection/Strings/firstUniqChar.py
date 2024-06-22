d17ced2d09eb0beb1002f8e7dd55ca735d681963
Adrian Molofsky <molofsky@stanford.edu>
2024-06-22 11:24:25 -0700

    Update firstUniqChar.py

--- Top Interview Questions/Easy Collection/Strings/firstUniqChar.py
+++ Top Interview Questions/Easy Collection/Strings/firstUniqChar.py
@@ -16,3 +16,14 @@ class Solution:
             if count[s[i]] == 1:
                 return i
         return -1
+
+        """
+        Solution # 2
+
+        for i in range(len(s)):
+            if s[i] not in s[:i] + s[i + 1:]:
+                return i
+        return -1
+        """
+
+        
