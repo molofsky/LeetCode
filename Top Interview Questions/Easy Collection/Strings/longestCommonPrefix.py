7701990c75d37c90956660899b4051e557fd7474
Adrian Molofsky <molofsky@stanford.edu>
2024-06-25 08:09:16 -0700

    Update longestCommonPrefix.py

--- Top Interview Questions/Easy Collection/Strings/longestCommonPrefix.py
+++ Top Interview Questions/Easy Collection/Strings/longestCommonPrefix.py
@@ -25,3 +25,20 @@ class Solution:
                 if j == len(strs) - 1: res += first_word[i]
             if (break_flag): break
         return res
+
+        """
+        Solution #2 
+
+        if strs is None:
+            return ""
+
+        min_len = min(len(s) for s in strs)
+        prefix = strs[0][:min_len]
+
+        for i in range(min_len):
+            for s in strs:
+                if s[i] != prefix[i]:
+                    return prefix[:i]
+                    
+        return prefix
+        """
