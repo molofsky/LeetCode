ae77ae3a9cfcfc20511b35a86116144b2c2da06a
Adrian Molofsky <molofsky@stanford.edu>
2024-06-28 17:28:18 -0700

    Update mergeTwoLists.py

--- Top Interview Questions/Easy Collection/Linked List/mergeTwoLists.py
+++ Top Interview Questions/Easy Collection/Linked List/mergeTwoLists.py
@@ -42,5 +42,27 @@ class Solution:
         
                 
         return new_head
-            
+
+        """
+        More elegant solution
+        
+        dummy = ListNode()
+        cur = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        
+        if list1:
+            cur.next = list1
+        elif list2:
+            cur.next = list2
+        
+        return dummy.next
+        """
         
