74e5b626714952c785a7c549ed0ece066531dd7d
Adrian Molofsky <molofsky@stanford.edu>
2024-07-01 19:30:15 -0700

    Update maxDepth.py

--- Top Interview Questions/Easy Collection/Trees/maxDepth.py
+++ Top Interview Questions/Easy Collection/Trees/maxDepth.py
@@ -13,3 +13,24 @@ class Solution:
         left_depth = self.maxDepth(root.left)
         
         return max(right_depth, left_depth) + 1
+
+    """
+    Solution #2 Iterative (BFS)
+
+    if root is None:
+            return 0
+            
+    count = 0
+    queue = [root]
+    while queue:
+        level_len = len(queue)
+        for _ in range(level_len):
+            node = queue.pop(0)
+
+            if node.right:
+                queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+        count += 1
+    return count
+    """
